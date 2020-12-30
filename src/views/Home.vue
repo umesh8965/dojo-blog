b425aefa6141bb36eeb7be050e62c640fa903c54
umesh8965 <umesh_8965@yahoo.co.in>
2020-12-31 00:25:55 +0530

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Home</h1>
+   <div v-if="error">{{ error }}</div>
+   <div v-if="posts.length">
+        <PostList :posts="posts" />
+   </div>
+   <div v-else>
+         <Spinner />
+   </div>
+ 
   </div>
 </template>
 
 <script>
+//import { ref } from 'vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostList from '../components/PostList'
+import getPosts from "../composables/getPosts"
+import Spinner from "../components/Spinner"
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    PostList, Spinner
+  },
+  setup(){
+
+    // ==== Warp up this code to composible function for reuse code === START ====
+       
+        // const posts = ref([])
+        // const error = ref(null)    
+        // const load = async () => {
+        //   try {
+        //     let data = await fetch('http://localhost:3000/posts')
+
+        //     if(!data.ok){
+        //       throw Error("No data found")
+        //     }
+
+        //     posts.value = await data.json()
+
+        //   } catch (err) {
+        //     error.value = err.message
+        //     console.log(error.value)
+        //   }
+        // }
+        // load()
+
+    //  === END ====
+
+
+    const { posts, error, load } = getPosts()
+
+    load()
+    
+    return { posts, error }
   }
 }
 </script>
+<style>
+  .home {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+</style>
